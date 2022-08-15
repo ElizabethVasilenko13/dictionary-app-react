b93b042b3d0e59c25c6cc32dcaa15915ea5a4803
Liza Vasilenko <liza.vasilenko13@gmail.com>
2022-08-15 16:19:56 +0200

    changed with one dedinition

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,14 +4,16 @@ export default function Meaning(props) {
 	return (
 		<div>
 			<h3>{props.meaning.partOfSpeech}</h3>
-			{props.meaning.definitions.map((definition, index) => {
+			<p>{props.meaning.definitions[0].definition}</p>
+			<em><p>{props.meaning.definitions[0].example}</p></em>
+			{/* {props.meaning.definitions.map((definition, index) => {
 				return (
 					<div key={index}>
 						<p>{definition.definition}</p>
 						<em><p>{definition.example}</p></em>
 					</div>
 				);
-			})}
+			})} */}
 		</div>
 	);
 }
