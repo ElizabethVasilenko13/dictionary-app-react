3db723f0bf1b38754ab6aca588d138ca757daa6f
Liza Vasilenko <liza.vasilenko13@gmail.com>
2022-08-15 21:03:36 +0200

    added audio phohetics

--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Meaning from './Meaning';
+import Phonetics from "./Phonetics";
 
 export default function Result(props) {
 	if(props.data) {
 		return (
 			<div className="result">
 				<h2>{props.data.word}</h2>
+				{props.data.phonetics.map((el, index) => {
+					return (
+						<div key={index}>
+							<p>{el.text}</p>
+							<Phonetics data={el}/>
+						</div>
+					);
+					
+				})}
+				
 				{props.data.meanings.map((el, index) => {
 					return (
 						<div key={index}>
